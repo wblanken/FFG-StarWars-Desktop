85d0f5602f4d24a0b478c8eec8d3cc3ad5222b9a
wblanken <blankenship.will@gmail.com>
2016-05-14 04:48:39 -0500

    Started work on the new model. Trying to simplify it a bit.

---
+++ OggDudeData/ImportData.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2016 Will Blankenship, Inc.  All Rights Reserved.
+
+using System.IO;
+using System.Xml.Serialization;
+using OggDudeData.Model;
+
+namespace OggDudeData
+{
+   public static class ImportData
+   {
+      public static Character LoadCharacter(string filePath)
+      {
+         var serializer = new XmlSerializer(typeof(Character));
+         using (var file = new FileStream(filePath, FileMode.Open,
+               FileAccess.Read))
+         {
+            var character = (Character)serializer.Deserialize(file);
+            return character;
+         }
+      }
+
+      public static void WriteCharacter(Character chracter)
+      {
+         var serializer = new XmlSerializer(typeof(Character));
+      }
+   }
+}
